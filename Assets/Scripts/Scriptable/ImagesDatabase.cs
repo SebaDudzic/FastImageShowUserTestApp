f4783f3b018b1cfa917ec74a4740d384e9d4f8a1
Unknown <seba.dudzic@gmail.com>
2017-05-12 22:05:21 +0200

    basic image showing logic

---
+++ Assets/Scripts/Scriptable/ImagesDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu(fileName = "Images_", menuName = "Images", order = 1)]
+public class ImagesDatabase : ScriptableObject {
+
+    [SerializeField]
+    private List<Sprite> images;
+
+    public Sprite GetRandomImage()
+    {
+        int index = Random.Range(0, images.Count);
+        return images[index];
+    }
+}
---
+++ Assets/Scripts/UIButtonsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonsController : MonoBehaviour {
+
+    [SerializeField]
+    private ImageFastShower imageFastShower;
+
+	public void OnShowFastImageClicked()
+    {
+        imageFastShower.Show();
+    }
+}
